de3fbbbaef618f72461dbedb4e0f14381011c837
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-11-19 07:47:52 -0500

    insert file notes for clarity

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+// global dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// initiate + set foundation for port 
 const PORT = 3000;
 
 const app = express();
 
+// middleware
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// set static files to public folder 
 app.use(express.static("public"));
 
+// initiate connection to MongoDB
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/columbia-university',
   {
@@ -28,6 +32,7 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
+// initiate listening on server / port
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
