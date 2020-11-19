163433c3eb21ce51b567976b1c59bc068cce4d66
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-11-18 21:33:34 -0500

    updates to pathways in html files

--- routes/view.js
+++ routes/view.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/exercise", (req, res) => {
+router.get("/public/exercise.html", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/stats", (req, res) => {
+router.get("/public/stats.html", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
